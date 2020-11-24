3b43a4cf741c883c32c63c0c45a2a946053ad5c3
AkdenizO <akdenizozal@gmail.com>
2020-11-24 20:33:29 +0100

    sepete ekleme kismi eklendi

--- src/test3.java
+++ src/test3.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class test3 {
+public class test3 {
+    public static void main(String[] args) {
+
+        System.out.println("birinci islem");
+    }
+    public static void yeniMethod()
+    {
+        System.out.println("yeni methodum hazir");
+        System.out.println("sepete ekle kisimlari buralar");
+    }
 }
