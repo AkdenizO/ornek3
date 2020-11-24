925e110e6c12d61b698be24a9993c5794ebefc9f
AkdenizO <akdenizozal@gmail.com>
2020-11-24 20:42:42 +0100

    sepetten sil yapildi sepete ekle fixlendi(noktali sayi düzeltme)

--- src/test3.java
+++ src/test3.java
@@ -5,7 +5,7 @@ public class test3 {
     }
     public static void yeniMethod()
     {
-        System.out.println("yeni methodum hazir");
+
         System.out.println("sepete ekle kisimlari buralar");
     }
     private static void sepettensil()
